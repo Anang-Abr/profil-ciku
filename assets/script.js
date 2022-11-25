c240e824a10d555156ba9c4a168255313f8a9cae
Anang-Abr <ananabr2610@gmail.com>
2022-11-26 00:16:01 +0800

    first commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const aboutButton = document.getElementById("about_button");
+
+aboutButton.onclick = function () {
+	const aboutCiku = document.querySelector(".about_ciku");
+	aboutCiku.classList.toggle("show");
+	if (aboutCiku.classList.contains("show")) {
+		aboutButton.innerHTML = "Gausah, gapeduli";
+	} else {
+		aboutButton.innerHTML = "Klik Disini";
+	}
+};
